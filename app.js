bc3f5aaaece6320bd545beff2ebee16ed6168c37
itachi <jlopex83@gmail.com>
2022-05-04 20:11:07 -0700

    1c new branch named jose created, just testing repo...

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ const express = require('express');
 const app = express();
 const port = 8000
 const mongoose = require('mongoose')
-const keys = require('./config/keys');
 const bodyParser = require('body-parser')
 const Todo = require('./models/todo');
 const todoRoutes = require('./routes/todos')
+// jose
+//var fs = require('fs');
+//var path = require('path');
+//require('dotenv/config');
+// Step 2 - connect to the database - jose
+/* mongoose.connect("mongodb://localhost:3306/CS664_Photos",
+	{ useNewUrlParser: true, useUnifiedTopology: true }, err => {
+		console.log('connected')
+	});
+ */
+// jose end 
 
+const keys = require('./config/keys');  
 
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
 
