c6cb342781b921134327d550a5422bc2c6104e6a
itachi <jlopex83@gmail.com>
2022-05-05 01:11:26 -0700

    4c app.js modified

--- app.js
+++ app.js
@@ -2,17 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8000
 const mongoose = require('mongoose')
+const keys = require('./config/keys');
 const bodyParser = require('body-parser')
-const Todo = require('./models/todo');
-const todoRoutes = require('./routes/todos')
-
-const keys = require('./config/keys');  
 const Photo = require('./models/photo');
-const photoRoutes = require('./routes/photos'); 
+const photoRoutes = require('./routes/photos')
+
 
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true})
 
 console.log(keys.mongoURI);
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
