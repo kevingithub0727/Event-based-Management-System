f719a56ecd98aa9bb6a91515d231fe437cb8c6e7
itachi <jlopex83@gmail.com>
2022-05-07 12:25:06 -0700

    5c added more images, more later...

--- routes/photos.js
+++ routes/photos.js
@@ -7,7 +7,15 @@ const Photo = require('../models/photo')
 router.post('/imageTitle', async (req, res)=>{
     console.log(req);
     const photos = await Photo.find(
-        { "imageTitle": { "$regex": req.body.inputField, "$options": "i" } } 
+        { "imageTitle": { "$regex": req.body.inputField, "$options": "i"} } 
+    );
+    res.json(photos)
+})
+
+router.post('/imageName', async (req, res)=>{
+    console.log(req);
+    const photos = await Photo.find(
+        { "imageName": { "$regex": req.body.inputField, "$options": "si"} } 
     );
     res.json(photos)
 })
@@ -15,7 +23,7 @@ router.post('/imageTitle', async (req, res)=>{
 router.post('/month', async (req, res)=>{
     console.log(req);
     const photos = await Photo.find(
-        { "month": { "$regex": req.body.inputField2, "$options": "i" } } 
+        { "month": { "$regex": req.body.inputField2, "$options": "si" } } 
     );
     res.json(photos)
 })
